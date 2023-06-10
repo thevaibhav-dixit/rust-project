4ab80cccd2b1b4c7c6a79a59b93b0d657946c7b1
Vaibhav <76673432+thevaibhav-dixit@users.noreply.github.com>
2023-06-10 17:10:08 +0530

    feat: add something

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,9 @@ fn main() {
     let ans = add_number(12, 12);
     println!("The answer is {}", ans);
 }
-
+fn multiplY(a : i32 , b : i32) -> i32 {
+    a*b
+}
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -13,4 +15,6 @@ mod tests {
     fn test_add_number() {
         assert_eq!(add_number(12, 12), 24);
     }
-}
+    fn test(){
+        assert_eq!(12, 12);
+    }
